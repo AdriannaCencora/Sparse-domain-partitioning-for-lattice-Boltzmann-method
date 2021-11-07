cf883593514a31e44c4f9c9af76abc3c39f40c5e
cencora <adrianna.cencora@nokia.com>
2021-11-08 00:59:56 +0100

    Cleanup, remove unused code

--- include/tile.h
+++ include/tile.h
@@ -23,14 +23,7 @@ struct tile_2d
     std::vector<coords_2d_t> hit_coords_;
 };
 
-struct tile_2d_remainder
-{
-    coords_2d_t dimensions_;
-    size_t number_of_hits_ = 0;
-    std::vector<coords_2d_t> hit_coords_;
-};
 typedef std::map<coords_2d_t, tile_2d> tiling_2d_map_t;
-typedef std::map<coords_2d_t, tile_2d_remainder> tiling_2d_remainders_map_t;
 
 struct tile_3d
 {
@@ -40,15 +33,7 @@ struct tile_3d
     std::vector<coords_3d_t> hit_coords_;
 };
 
-struct tile_3d_remainder
-{
-    coords_3d_t dimensions_;
-    size_t number_of_hits_ = 0;
-    std::vector<coords_3d_t> hit_coords_;
-};
-
 typedef std::map<coords_3d_t, tile_3d> tiling_3d_map_t;
-typedef std::map<coords_3d_t, tile_3d_remainder> tiling_3d_remainders_map_t;
 
 struct tiling_2d_parameters_store
 {
@@ -69,13 +54,9 @@ struct tiling_3d_parameters_store
 
     tiling_3d_map_t empty_tiles_;
     tiling_3d_map_t non_empty_tiles_;
-    tiling_3d_remainders_map_t empty_remainders_;
-    tiling_3d_remainders_map_t non_empty_remainders_;
 
     size_t total_hits_ = 0;
     float total_hit_ratio_ = 0.0;
-
-    size_t number_of_missed_hits_ = 0; // hits in remainder
 };
 
 typedef boost::variant<tiling_2d_parameters_store,
