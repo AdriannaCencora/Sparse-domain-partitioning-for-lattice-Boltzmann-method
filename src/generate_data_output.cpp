974f3023dd7a53052c1a51c9ff04878000e78f79
cencora <adrianna.cencora@nokia.com>
2022-01-16 23:26:51 +0100

    Generating json output finished

--- src/generate_data_output.cpp
+++ src/generate_data_output.cpp
@@ -2,11 +2,13 @@
 
 #include <nlohmann/json.hpp>
 
-using json = nlohmann::json;
+#include <iomanip>
+
+using json = nlohmann::ordered_json;
 
 void generate_data_output(tiling_parameters_store_variant_t& data_store)
 {
-    std::string filename = "tiling_parameters";
+    std::string filename = "tiling_parameters.json";
     std::fstream output_file;
 
     output_file.open(filename, std::ios::out | std::ios::app);
@@ -25,39 +27,33 @@ void generate_data_output(tiling_parameters_store_variant_t& data_store)
 
 void file_writer::operator()(const tiling_parameters_store<coords_2d>& data_store)
 {
-    output_file_ << "GEOMETRY 2D - TILING PARAMETERS" << std::endl;
 
-    output_file_ << "Tile size: " << data_store.tile_size_ << std::endl;
-    output_file_ << "Offset: " << data_store.offset_.x << ", " <<
-        data_store.offset_.y << std::endl;
-    output_file_ << "Non empty tiles: " << data_store.tiles_.size() << std::endl;
+    json parameters;
+    parameters["tile_size"] = data_store.tile_size_;
+    parameters["offset"] = {data_store.offset_.x, data_store.offset_.y};
+    parameters["tiles"] = data_store.tiles_.size();
+    parameters["total_hits"] = data_store.total_hits_;
+    parameters["fill_ratio"] = data_store.fill_ratio_;
+    parameters["common_edges"] = data_store.number_of_common_edges_;
+    parameters["common_edges_ratio"] = data_store.common_edges_ratio_;
+    parameters["common_vertices"] = data_store.number_of_common_vertices_;
+    parameters["common_vertices_ratio"] = data_store.common_vertices_ratio_;
 
-    output_file_ << "Total hits: " << data_store.total_hits_ << std::endl;
-    output_file_ << "Fill ratio: " << data_store.fill_ratio_ << std::endl;
-    output_file_ << "Number of common edges: " << data_store.number_of_common_edges_ << std::endl;
-    output_file_ << "Common edges ratio: " << data_store.common_edges_ratio_ << std::endl;
-    output_file_ << "Number of common vertices " << data_store.number_of_common_vertices_ << std::endl;
-    output_file_ << "Common vertices ratio: " << data_store.common_vertices_ratio_ << std::endl;
-
-     output_file_ << std::endl << std::endl;
+    output_file_ << std::setw(4) << std::setprecision(5) << parameters << "," << std::endl;
 }
 
 void file_writer::operator()(const tiling_parameters_store<coords_3d>& data_store)
 {
-    output_file_ << "GEOMETRY 3D - TILING PARAMETERS" << std::endl;
+    json parameters;
+    parameters["tile_size"] = data_store.tile_size_;
+    parameters["offset"] = {data_store.offset_.x, data_store.offset_.y, data_store.offset_.z};
+    parameters["tiles"] = data_store.tiles_.size();
+    parameters["total_hits"] = data_store.total_hits_;
+    parameters["fill_ratio"] = data_store.fill_ratio_;
+    parameters["common_edges"] = data_store.number_of_common_edges_;
+    parameters["common_edges_ratio"] = data_store.common_edges_ratio_;
+    parameters["common_vertices"] = data_store.number_of_common_vertices_;
+    parameters["common_vertices_ratio"] = data_store.common_vertices_ratio_;
 
-    output_file_ << "Tile size: " << data_store.tile_size_ << std::endl;
-    output_file_ << "Offset xyz: " << data_store.offset_.x << ", " <<
-        data_store.offset_.y<< ", "<<
-        data_store.offset_.z<<  std::endl;
-    output_file_ << "Non empty tiles: " << data_store.tiles_.size() << std::endl;
-
-    output_file_ << "Total hits: " << data_store.total_hits_ << std::endl;
-    output_file_ << "Total hit ratio: " << data_store.fill_ratio_ << std::endl;
-    output_file_ << "Number of common edges: " << data_store.number_of_common_edges_ << std::endl;
-    output_file_ << "Common edges ratio: " << data_store.common_edges_ratio_ << std::endl;
-    output_file_ << "Number of common vertices " << data_store.number_of_common_vertices_ << std::endl;
-    output_file_ << "Common vertices ratio: " << data_store.common_vertices_ratio_ << std::endl;
-
-     output_file_ << std::endl << std::endl;
+    output_file_ << std::setw(4) << std::setprecision(5) << parameters << "," << std::endl;
 }
